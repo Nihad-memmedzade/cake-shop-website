a4d3385cbb3896bd282ad8c15cc4f93daca67ea1
Nihad-memmedzade <nihad.mmmdzad@bk.ru>
2026-05-24 02:17:43 +0200

    Add updated Product detail page

--- src/pages/shop/details/sections/relatedProducts/index.tsx
+++ src/pages/shop/details/sections/relatedProducts/index.tsx
@@ -1,19 +1,20 @@
 import { useEffect } from "react";
-import { Autoplay, FreeMode, Navigation, Pagination } from "swiper/modules";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
+
+import PageLoader from "@/assets/components/pageLoader/pageLoader";
 import ProductCard from "@/assets/components/productCard/productCard";
-import { useAppDispatch, useAppSelector, type RootState } from "@/store/store";
 import {
   clearRelatedProducts,
   fetchRelatedProducts,
 } from "@/store/productSlice";
+import { useAppDispatch, useAppSelector, type RootState } from "@/store/store";
+
 import styles from "./relatedProducts.module.scss";
 
 import "swiper/css";
-import "swiper/css/free-mode";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
-import PageLoader from "@/assets/components/pageLoader/pageLoader";
 
 type RelatedProductsProps = {
   currentProductId: number;
@@ -24,11 +25,8 @@ export default function RelatedProducts({
 }: RelatedProductsProps) {
   const dispatch = useAppDispatch();
 
-  const {
-    relatedProducts,
-    relatedProductsLoading,
-    relatedProductsError,
-  } = useAppSelector((state: RootState) => state.products);
+  const { relatedProducts, relatedProductsLoading, relatedProductsError } =
+    useAppSelector((state: RootState) => state.products);
 
   useEffect(() => {
     dispatch(fetchRelatedProducts({ productId: currentProductId, limit: 8 }));
@@ -38,15 +36,14 @@ export default function RelatedProducts({
     };
   }, [dispatch, currentProductId]);
 
-if (relatedProductsLoading) {
-  return (
-    <PageLoader
-      title="Loading related cakes"
-      text="Finding similar cakes for you."
-    />
-  );
-}
-
+  if (relatedProductsLoading) {
+    return (
+      <PageLoader
+        title="Loading related cakes"
+        text="Finding similar cakes for you."
+      />
+    );
+  }
 
   if (relatedProductsError) {
     return (
@@ -72,29 +69,56 @@ if (relatedProductsLoading) {
         </p>
       </div>
 
-      <Swiper
-        spaceBetween={18}
-        loop={relatedProducts.length > 4}
-        freeMode
-        pagination={{ clickable: true }}
-        navigation
-        autoplay={{ delay: 4500, disableOnInteraction: false }}
-        speed={900}
-        modules={[Autoplay, FreeMode, Pagination, Navigation]}
-        className={`${styles.relatedSwiper} relatedProductsSwiper`}
-        breakpoints={{
-          0: { slidesPerView: 1.15, spaceBetween: 14 },
-          480: { slidesPerView: 2.05, spaceBetween: 16 },
-          768: { slidesPerView: 3, spaceBetween: 18 },
-          1024: { slidesPerView: 4, spaceBetween: 18 },
-        }}
-      >
-        {relatedProducts.map((item) => (
-          <SwiperSlide key={item.id} className={styles.slide}>
-            <ProductCard card={item} />
-          </SwiperSlide>
-        ))}
-      </Swiper>
+      <div className={styles.relatedSliderShell}>
+        <button
+          type="button"
+          className={`${styles.relatedNav} ${styles.relatedNavPrev} relatedProductsPrev`}
+          aria-label="Previous related products"
+        />
+
+        <Swiper
+          modules={[Autoplay, Pagination, Navigation]}
+          className={`${styles.relatedSwiper} relatedProductsSwiper`}
+          slidesPerView="auto"
+          spaceBetween={14}
+          loop={relatedProducts.length > 4}
+          watchOverflow
+          navigation={{
+            prevEl: ".relatedProductsPrev",
+            nextEl: ".relatedProductsNext",
+          }}
+          pagination={{ clickable: true }}
+          autoplay={{
+            delay: 4500,
+            disableOnInteraction: false,
+            pauseOnMouseEnter: true,
+          }}
+          speed={800}
+          breakpoints={{
+            0: {
+              spaceBetween: 10,
+            },
+            576: {
+              spaceBetween: 12,
+            },
+            992: {
+              spaceBetween: 14,
+            },
+          }}
+        >
+          {relatedProducts.map((item) => (
+            <SwiperSlide key={item.id} className={styles.slide}>
+              <ProductCard card={item} variant="compact" />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+
+        <button
+          type="button"
+          className={`${styles.relatedNav} ${styles.relatedNavNext} relatedProductsNext`}
+          aria-label="Next related products"
+        />
+      </div>
     </section>
   );
 }
